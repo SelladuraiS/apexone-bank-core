83e6a7274e02a92205a99543acf814e787c6ae42
Selladurai S <139627856+SelladuraiS@users.noreply.github.com>
2026-01-04 12:26:06 +0530

    Initial Spring Boot project setup for ApexOne Bank

---
+++ src/main/java/com/apexonebank/core/ApexoneBankCoreApplication.java
@@ -0,0 +1,13 @@
+package com.apexonebank.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApexoneBankCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApexoneBankCoreApplication.class, args);
+	}
+
+}
