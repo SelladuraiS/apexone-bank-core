270036af3281c9a3d4d75284c265445b2c668a03
Selladurai S <139627856+SelladuraiS@users.noreply.github.com>
2026-01-04 22:17:40 +0530

    chore: configure H2 datasource and bootstrap product data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO products
 (id, product_name, product_type, allow_credit, allow_debit, minimum_balance, interest_applicable, active)
 VALUES
-(1, 'Savings Account', 'SAVINGS', true, true, 1000, true, true),
-(2, 'Current Account', 'Current Account', 'CURRENT', true, true, 0, false, true),
-(3, 'Fixed Deposit', 'FD', true, false, 0, true, true);
+(1, 'Savings Account', 'SAVINGS_ACCOUNT', true, true, 1000, true, true),
+(2, 'Current Account', 'CURRENT_ACCOUNT', true, true, 0, false, true),
+(3, 'Fixed Deposit', 'FIXED_DEPOSIT', true, false, 0, true, true);
